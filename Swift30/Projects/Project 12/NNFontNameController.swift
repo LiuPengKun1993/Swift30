a78940dc5c42c37e5a8eae2320557a83c3153851
刘朋坤 <pengkun@7net.cc>
2020-03-04 17:00:22 +0800

    Font Names

--- Swift30/Projects/Project 12/NNFontNameController.swift
+++ Swift30/Projects/Project 12/NNFontNameController.swift
@@ -1,26 +1,24 @@
 //
-//  ViewController.swift
+//  NNFontNameController.swift
 //  Swift30
 //
-//  Created by liupengkun on 2020/1/8.
-//  Copyright © 2020 liupengkun. All rights reserved.
+//  Created by 彭倩倩 on 2020/3/4.
+//  Copyright © 2020 刘朋坤. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
-    
-    var classNames = ["NNStopWatchController", "NNBatteryController", "NNValidationCodeController", "NNValidationController", "NNWeChatReadController", "NNWebViewController", "NNTextViewController", "NNReplyCommentViewController", "NNPickerViewController", "NNSelectionBoxViewController", "NNDrawingBoardController", "NNFontNameController"]
-    var titleNames = ["01 计时器", "02 电池条", "03 圆形验证码输入框", "04 图形验证码", "05 UICollectionView练习&微信读书翻页动画", "06 WKWebView练习&JS原生交互", "07 带占位文本且自动换行的TextView", "08 自定义评论框", "09 选择器 UIPickView", "10 选择框", "11 画板", "12 Font Names"]
-    
+class NNFontNameController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.title = "Font Names"
         view.addSubview(baseView)
     }
     
     // MARK: cell 个数
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return titleNames.count
+        return UIFont.familyNames.count
     }
     
     // MARK: cell 内容
@@ -31,16 +29,16 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
             cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellid)
         }
         cell?.selectionStyle = UITableViewCell.SelectionStyle.gray
-        cell?.textLabel?.text = titleNames[indexPath.row]
+        let name = UIFont.familyNames[indexPath.row]
+        cell?.textLabel?.text = "hello Swift"
+        cell?.detailTextLabel?.text = name
+        cell?.textLabel?.font = UIFont(name: name, size: 20)
         return cell!
     }
     
     // MARK: cell 点击
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let class_VC = NSClassFromString("Swift30.\(classNames[indexPath.row])") as! UIViewController.Type
-        let vc = class_VC.init()
-        self.navigationController?.pushViewController(vc, animated: true)
     }
     
     // MARK: 懒加载列表视图
